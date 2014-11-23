8199daf1899420c42d180e81e6cfaac22006f9d9
Basti25 <ich250@freenet.de>
2014-11-23 12:30:49 +0100

    - removed unused + added sound + added functions

--- Game/run.py
+++ Game/run.py
@@ -1,5 +1,6 @@
 __author__ = 'Basti'
 
+import random
 from pygame import *
 from pygame.sprite import *
 
@@ -11,42 +12,36 @@ def main():
     size = width, height = 640, 480
     screen = pygame.display.set_mode(size)
     screen.fill((255, 255, 255))
-    pygame.display.set_caption('Paint Brush')
+    pygame.display.set_caption('Hit the mole!')
+    pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 
-    # Entities
-    # bg = pygame.image.load('./images/world.jpg') # Background images
-    # screen.blit(bg, (0, 0))
+    score = 0
+    black = (0, 0, 0)
+    white = (255, 255, 255)
 
-    black_brush = pygame.image.load('./images/blackBrush.gif')
-    yellow_brush = pygame.image.load('./images/yellowBrush.gif')
-    red_brush = pygame.image.load('./images/redBrush.gif')
-
-    black_brush = pygame.transform.scale(black_brush, (32, 32))
-    yellow_brush = pygame.transform.scale(yellow_brush, (32, 32))
-    red_brush = pygame.transform.scale(red_brush, (32, 32))
-
-    brush = black_brush
-    # brush = pygame.transform.scale(brush, (64, 64))
-    xPos = yPos = 16
-    # brushRect = brush.get_rect()
     pygame.display.update()
+
+    collideSound = pygame.mixer.Sound('./sounds/collide.wav')
 
     # Loop == ALTER
     # Assign Values to key variables
     keepGoing = True
-    paint = False
     clock = pygame.time.Clock()
 
-    ball = Ball()
+    hitted_the_mole = False
 
-    sprites = RenderPlain(ball)
+    mole = Mole()
 
-    sprites.draw(screen)
+    sprites = RenderPlain(mole)
+    lastMovementOfTheMole = 0
+
+    cursor_picture = pygame.image.load('./images/schaufel.png').convert_alpha()
 
     # Loop
     while keepGoing:
         # Timing
         clock.tick(30)
+        actualTimeInSeconds = int(round(pygame.time.get_ticks()/1000))
 
         # Events
         x, y = pygame.mouse.get_pos()
@@ -55,41 +50,48 @@ def main():
                 keepGoing = False
                 break
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if ball.rect.collidepoint(mouse.get_pos()):
-                    print('collide')
-                paint = True
-            elif event.type == pygame.MOUSEBUTTONUP:
-                paint = False
+                if mole.rect.collidepoint(mouse.get_pos()) and not hitted_the_mole:
+                    hitted_the_mole = True
+                    collideSound.play()
+                    score += 1
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
                 pygame.image.save(screen, "screenshot.jpg")
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
-                brush = black_brush
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
-                brush = red_brush
-            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_y or event.key == pygame.K_z):
-                brush = yellow_brush
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_p:
-                brush = pygame.transform.scale(brush, (32, 32))
-                xPos = yPos = 16
-            elif event.type == pygame.KEYDOWN and event.key == pygame.K_m:
-                brush = pygame.transform.scale(brush, (16, 16))
-                xPos = yPos = 8
 
-        # Refresh Display
-        if paint:
-            # screen.blit(bg, (0, 0))
-            screen.blit(brush, (x-xPos, y-yPos))
-            pygame.display.update()
+        screen.fill(white)
+
+        sprites.draw(screen)
+        screen.blit(cursor_picture, pygame.mouse.get_pos())
+
+        font = pygame.font.SysFont(None, 25)
+        text = font.render("Score: " + str(score), True, black)
+        screen.blit(text, (0, 0))
+
+        # change mole position after every second
+        if lastMovementOfTheMole != actualTimeInSeconds:
+            lastMovementOfTheMole = actualTimeInSeconds
+
+            hitted_the_mole = False
+            mole.move(random.randint(0, width - mole.rect.width), random.randint(0, height - mole.rect.height))
+
+        pygame.display.update()
 
 
-class Ball(Sprite):
+class Mole(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = image.load("./images/yellowBrush.gif").convert()
+        MoleImage = image.load("./images/mole.png")
+
+        self.image = MoleImage.convert_alpha()
+        self.width = self.image.get_width
+        self.height = self.image.get_height
         self.rect = self.image.get_rect()
 
     def update(self):
         self.rect.center = mouse.get_pos()
+
+    def move(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
 
 if __name__ == '__main__':
     main()
